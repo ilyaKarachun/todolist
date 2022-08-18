2178e2dc6699785cff02c0fb8ee4669e587d62b4
Ilya_Karachun <Karachun_ilya94@mail.ru>
2022-08-18 19:12:04 +0300

    Updates

---
+++ precache-manifest.832033f79cf835765e7324f10ea28d00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25a56bfab895503ba46f33e2928f9cf",
+    "url": "/samurai-way/index.html"
+  },
+  {
+    "revision": "8d42a594f07b9abf13a9",
+    "url": "/samurai-way/static/css/main.dbad68f3.chunk.css"
+  },
+  {
+    "revision": "9e7753b88597ae6c19d7",
+    "url": "/samurai-way/static/js/2.b6fe17b3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/samurai-way/static/js/2.b6fe17b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d42a594f07b9abf13a9",
+    "url": "/samurai-way/static/js/main.9ee18cef.chunk.js"
+  },
+  {
+    "revision": "cfd6e803999af8d60bb3",
+    "url": "/samurai-way/static/js/runtime-main.a83e9af1.js"
+  }
+]);
